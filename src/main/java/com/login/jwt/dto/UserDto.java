213a61d36610a9aec5958f3bb14a45edc722b6fc
sujin lim <swuujin990827@gmail.com>
2022-10-31 22:10:26 +0900

    오류 수정

--- src/main/java/com/login/jwt/dto/UserDto.java
+++ src/main/java/com/login/jwt/dto/UserDto.java
@@ -20,16 +20,13 @@ import lombok.NoArgsConstructor;
 public class UserDto {
 
 	@NotNull
-	@Size(min=3, max=50)
 	private String username;
 
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@NotNull
-	@Size(min=3, max=100)
 	private String password;
 
 	@NotNull
-	@Size(min=3, max=50)
 	private String nickname;
 
 
